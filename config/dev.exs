ef8b238e4819dac8c1e5d8d098cd0c09fb802fe1
manaranq <manara.quintino@deliverycenter.com>
2021-09-10 19:25:52 -0300

    feature Link Novas Tarefas

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :kanbax, Kanbax.Repo,
   username: "postgres",
   password: "postgres",
   database: "desafio2_dev",
-  hostname: "db",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
